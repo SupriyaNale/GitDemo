f77eb7939af5af6f0ea3af7822272d260cb3442b
SupriyaNale <supriyanale42@gmail.com>
2016-10-03 13:42:13 +0530

    first commit thr terminal

---
+++ GitDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitDemo
+//
+//  Created by Supriya on 03/10/16.
+//  Copyright © 2016 Redbytes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
